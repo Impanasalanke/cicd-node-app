0428b7dce54e470d74234611dc702ec9bcc5f446
Impanasalanke <impanasalanke20@gmail.com>
2026-04-09 19:47:14 +0530

    updated app.js

--- src/app.js
+++ src/app.js
@@ -41,7 +41,7 @@ app.use((req, res) => {
 
 if (process.env.NODE_ENV !== 'test') {
   app.listen(PORT, () => {
-    console.log(`✅ Server running on http://localhost:${PORT}`);
+    console.log(` Server running on http://localhost:${PORT}`);
   });
 }
 
